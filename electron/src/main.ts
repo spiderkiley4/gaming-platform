f9c1291a633a9c336a28625d93e100093c7a3ee0
Jeremy Kiley <spiderkiley4@gmail.com>
2025-04-18 12:13:09 -0400

    Auto updating

--- electron/src/main.ts
+++ electron/src/main.ts
@@ -1,9 +1,14 @@
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, dialog } from 'electron';
 import * as path from 'path';
+import { autoUpdater } from 'electron-updater';
 
 const isDev = process.env.NODE_ENV === 'development';
 
 console.log('Electron main process started!');
+
+// Configure auto-updater
+autoUpdater.autoDownload = false;
+autoUpdater.autoInstallOnAppQuit = true;
 
 function createWindow() {
     const mainWindow = new BrowserWindow({
@@ -26,10 +31,51 @@ function createWindow() {
         // In production, load from the frontend build
         mainWindow.loadFile(path.join(__dirname, 'frontend', 'index.html'));
     }
+
+    return mainWindow;
 }
 
+// Auto-updater events
+autoUpdater.on('checking-for-update', () => {
+    console.log('Checking for updates...');
+});
+
+autoUpdater.on('update-available', (info) => {
+    dialog.showMessageBox({
+        type: 'info',
+        title: 'Update Available',
+        message: `Version ${info.version} is available. Would you like to download it now?`,
+        buttons: ['Yes', 'No']
+    }).then((buttonIndex) => {
+        if (buttonIndex.response === 0) {
+            autoUpdater.downloadUpdate();
+        }
+    });
+});
+
+autoUpdater.on('update-downloaded', (info) => {
+    dialog.showMessageBox({
+        type: 'info',
+        title: 'Update Ready',
+        message: `Version ${info.version} has been downloaded. The application will restart to install the update.`,
+        buttons: ['Restart Now', 'Later']
+    }).then((buttonIndex) => {
+        if (buttonIndex.response === 0) {
+            autoUpdater.quitAndInstall();
+        }
+    });
+});
+
+autoUpdater.on('error', (err) => {
+    dialog.showMessageBox({
+        type: 'error',
+        title: 'Update Error',
+        message: `Error in auto-updater: ${err.message}`
+    });
+});
+
 app.whenReady().then(() => {
-    createWindow();
+    const mainWindow = createWindow();
 
     if (isDev) {
         // Install React DevTools in development
@@ -41,6 +87,9 @@ app.whenReady().then(() => {
         } catch (e) {
             console.error('Error installing devtools:', e);
         }
+    } else {
+        // Check for updates when the app starts
+        autoUpdater.checkForUpdates();
     }
 
     app.on('activate', () => {
