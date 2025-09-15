9eb17c72435b4564c4271bffe9e80d3f5723937f
spiderkiley4 <spiderkiley4@gmail.com>
2025-09-15 10:05:26 -0400

    mobile update

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -2,21 +2,4 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// Configure logging
-config.logger = {
-  info: console.log,
-  warn: console.warn,
-  error: console.error,
-  debug: console.log, // Enable debug logs
-};
-
-// Don't filter any logs
-config.reporter = {
-  ...config.reporter,
-  update: () => {},
-  log: (message) => {
-    console.log(message);
-  },
-};
-
 module.exports = config;
